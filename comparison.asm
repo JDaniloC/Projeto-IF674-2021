a6746e5d7d57c9a2cab9fa46adad2bd6441f8c95
Danilo Silva <jdsc@cin.ufpe.br>
2021-10-05 22:29:27 -0300

    First exercises

---
+++ comparison.asm
@@ -0,0 +1,52 @@
+.data
+	askJoaoOld: .asciiz "Digite a idade de João: "
+	askPedroOld: .asciiz "Digite a idade de Pedro: "
+	
+	joaoIsOlderStr: .asciiz "João é mais velho que Pedro"
+	pedroIsOlderStr: .asciiz "Pedro é mais velho que João"
+	areEqualStr: .asciiz "Eles têm a mesma idade"
+.text  
+	li $v0, 4 # Carregando e printando uma frase
+	la $a0, askJoaoOld
+	syscall
+	
+	li $v0, 5 # Pedindo input do usuário
+	syscall
+	move $s0, $v0 # Movendo o valor para $s0
+	
+	li $v0, 4
+	la $a0, askPedroOld
+	syscall
+	
+	li $v0, 5
+	syscall
+	move $s1, $v0
+	
+	# Se for igual vai para o isEqual
+	beq $s0, $s1, isEqual
+	slt $t0, $s0, $s1 # É menor?
+	
+	beq $t0, $zero, isJoaoOlder
+	j isPedroOlder # Jump para isPedroOlder
+	
+end: # Finaliza o programa
+	li $v0, 10
+	syscall
+
+isJoaoOlder:
+	li $v0, 4
+	la $a0, joaoIsOlderStr
+	syscall 
+	j end
+
+isPedroOlder:
+	li $v0, 4
+	la $a0, pedroIsOlderStr
+	syscall 
+	j end
+
+isEqual:
+	li $v0, 4
+	la $a0, areEqualStr
+	syscall
+	j end
---
+++ loop.asm
@@ -0,0 +1,59 @@
+.data
+	askFirstTxt: .asciiz "Digite o primeiro valor: "
+	askSecondTxt: .asciiz "Digite o segundo valor: "
+	storedValue: .word 0
+
+.text
+	jal askFirstInput
+	move $s0, $v0
+	
+	jal askSecondInput
+	move $s1, $v0
+	
+	beq $s0, $zero, aIsZero
+	bne $s1, $zero, bIsNotZero
+	sw $s0, storedValue
+	j end
+	
+aIsZero:
+	sw $s1, storedValue
+	j end	
+
+bIsNotZero:
+	sle $t0, $s0, $s1
+	beq $t0, $zero, decreaseA
+	j decreaseB
+
+decreaseA:
+	sub $s0, $s0, $s1
+	bne $s1, $zero, bIsNotZero
+	j storeA
+
+decreaseB:
+	sub $s1, $s1, $s0
+	bne $s1, $zero, bIsNotZero
+
+storeA:
+	sw $s0, storedValue
+	j end
+
+askFirstInput:
+	li $v0, 4
+	la $a0, askFirstTxt
+	syscall
+	
+	j askValue
+
+askSecondInput:
+	li $v0, 4
+	la $a0, askSecondTxt
+	syscall
+
+askValue:
+	li $v0, 5
+	syscall
+	jr $ra
+
+end:
+	li $v0, 10
+	syscall
