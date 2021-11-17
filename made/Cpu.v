ce92c06bbe976064e998872f506658acb4ba3198
Danilo Silva <jdsc@cin.ufpe.br>
2021-11-17 01:37:39 -0300

    add some muxes

--- made/Cpu.v
+++ made/Cpu.v
@@ -4,9 +4,10 @@ module Cpu (
 )
     // Sinais de controle
 
-    wire I_or_D;
+    wire [2:0] I_or_D;
     wire ir_write;
     wire reg_write;
+    wire epc_write;
     wire read_or_write;
     wire mem_data_write;
     
@@ -17,12 +18,15 @@ module Cpu (
     wire [2:0] alu_op;
     wire [2:0] alu_src_a;
     wire [2:0] alu_src_b;
+    wire alu_out_control;
     wire [2:0] alu_control;
     
     wire [2:0] exp_control;
     wire [2:0] shift_control;
+    wire [2:0] shift_src_control;
+    wire [2:0] shift_amount_control;
     
-    wire [2:0] reg_dist;
+    wire [2:0] reg_dist_ctrl;
     wire [3:0] mem_to_reg;
     
     wire div_or_mult;
@@ -38,9 +42,13 @@ module Cpu (
     
     wire [31:0] pc_source_out;
     wire [31:0] pc_out;
+    wire [31:0] epc_out;
+    wire [31:0] exp_out;
+    wire [31:0] i_or_d_out;
 
     wire [31:0] alu_src_a_out;
     wire [31:0] alu_src_b_out;
+    wire [31:0] alu_reg_out;
     wire [31:0] alu_out;
     
     wire [4:0] reg_dist_out;
@@ -60,6 +68,11 @@ module Cpu (
     wire [31:0] mult_div_hi_out;
     wire [31:0] mult_div_lo_out;
 
+    wire [31:0] sign_extend_16to32_out;
+    wire [31:0] shift_left_16_out;
+    wire [31:0] shift_right_2_out;
+    wire [31:0] 28_to_32_out;
+
     // Resultados da ULA
 
     wire NG;
@@ -76,11 +89,6 @@ module Cpu (
     wire [4:0]  RT;
     wire [15:0] IMMEDIATE;
 
-    // Fios de dados com 32 bits
-    
-    wire [31:0] sign_extend_16to32_out;
-    wire [31:0] shift_left_mult_4_out;
-
     // Bloco central
 
     CtrlUnit cpu_ctrl (
@@ -88,7 +96,6 @@ module Cpu (
         .reset(reset),
         .I_or_D(I_or_D),
         .alu_op(alu_op),
-        .reg_dist(reg_dist),
         .ir_write(ir_write),
         .pc_write(pc_write),
         .reg_write(reg_write),
@@ -101,6 +108,7 @@ module Cpu (
         .div_control(div_control)
         .exp_control(exp_control),
         .alu_control(alu_control),
+        .reg_dist_ctrl(reg_dist_ctrl),
         .shift_control(shift_control),
         .read_or_write(read_or_write),
     );
@@ -221,38 +229,85 @@ module Cpu (
         .Saida(b_out)
     );
 
-    Registrador ALUOut_(
+    Registrador alu_out_reg (
         .Clk(clock),
         .Reset(reset),
-        ALUOutControl, // chart notation
-        ALU_out,
-        ALUOut_out
+        .Load(alu_out_control),
+        .Entrada(alu_reg_out),
+        
+        .Saida(alu_out)
     );
 
-    Registrador EPC_(
+    Registrador epc_reg (
         .Clk(clock),
         .Reset(reset),
-        EPCWrite, // chart notation
-        ALU_out,
-        EPC_out
+        .Load(epc_write),
+        .Entrada(alu_reg_out),
+        
+        .Saida(epc_out)
     );
 
     // Blocos de controle  
 
-    Mux2Bits AluScrA_(
-        .alu_src_a,
-        .pc_out,
-        .a_out,
-        .memory_data_out,
-        .alu_src_a_out
+    Mux2Bits mux_alu_src_a (
+        .seletor(alu_src_a),
+        .data_0(pc_out),
+        .data_1(a_out),
+        .data_output(alu_src_a_out),
     );
 
-    Mux2Bits AluScrB_(
-        .alu_scr_b,
-        .b_out,
-        .sign_extend_16to32_out,
-        .shift_left_mult_4_out,
-        .alu_scr_b_out
+    Mux4Bits mux_alu_src_b (
+        .seletor(alu_scr_b),
+        .data_0(b_out),
+        .data_1(2'b11),
+        .data_2(shift_left_16_out),
+        .data_3(shift_right_2_out),
+        .data_output(alu_scr_b_out)
+    );
+
+    Mux4Bits mux_i_or_d (
+        .seletor(I_or_D),
+        .data_0(pc_out),
+        .data_1(alu_out),
+        .data_2(from_div),
+        .data_3(exp_out),
+        .data_output(i_or_d_out),
+    );
+
+    Mux4Bits shift_src(
+        .seletor(shift_src_control),
+        .data_0(a_out),
+        .data_1(b_out),
+        .data_2(IMMEDIATE),
+        .data_3(a_out),
+        .data_output(shift_src_out),
+    );
+
+    Mux4Bits mux_shift_amount (
+        .seletor(shift_amount_control),
+        .data_0(2'b10000),
+        .data_1(memory_data_out),
+        .data_2(b_out),
+        .data_3(IMMEDIATE), // This is less
+        .data_output(shift_amount_out),
+    );
+
+    Mux4Bits mux_pc_source (
+        .seletor(pc_source),
+        .data_0(alu_out),
+        .data_1(alu_reg_out),
+        .data_2(28_to_32_out),
+        .data_3(epc_out),
+        .data_output(PCSource_out),
+    );
+
+    Mux4Bits reg_dist (
+        .seletor(reg_dist_ctrl),
+        .data_0(RS),
+        .data_1(10'31),
+        .data_2(10'30),
+        .data_3(IMMEDIATE),
+        .data_output(reg_dist_out),
     );
 
 endmodule
