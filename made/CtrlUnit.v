01a9b67d3f1e8fad8e88c401c3907d0d4fec97f9
Sofiamdl <sml@cin.ufpe.br>
2021-11-18 20:52:01 -0300

    feature(base-project): fix ctrl unit

--- made/CtrlUnit.v
+++ made/CtrlUnit.v
@@ -1,36 +1,37 @@
 module CtrlUnit (
     input clock,
     input reset,
-    output reg I_or_D;
-    output reg ir_write;
-    output reg reg_write;
+    output reg I_or_D,
+    output reg ir_write,
+    output reg reg_write,
     
-    output reg mem_read;
-    output reg read_or_write;
+    output reg mem_read,
+    output reg read_or_write,
     
-    output reg pc_write;
-    output reg pc_control;
-    output reg [2:0] pc_source;
+    output reg pc_write,
+    output reg pc_control,
+    output reg [2:0] pc_source,
     
-    output reg [2:0] alu_op;
-    output reg [2:0] alu_src_a;
-    output reg [2:0] alu_src_b;
+    output reg [2:0] alu_op,
+    output reg [2:0] alu_src_a,
+    output reg [2:0] alu_src_b,
     
-    output reg [2:0] exp_control;
-    output reg [2:0] shift_control;
+    output reg [2:0] exp_control,
+    output reg [2:0] shift_control,
     
-    output reg [2:0] reg_dist;
-    output reg [3:0] mem_to_reg;
+    output reg [2:0] reg_dist,
+    output reg [3:0] mem_to_reg,
     
-    output reg div_or_mult;
-    output reg div_control;
-    output reg [6:0] rst_out, 
-)
-  reg [1:0] STATE
-  reg [2:0] COUNTER
+    output reg div_or_mult,
+    output reg div_control,
+    output reg [6:0] rst_out
+  );
+
+  reg [1:0] STATE;
+  reg [2:0] COUNTER;
 
   parameter RESET = 6'b11111111;
-  parameter ST _RESET = 2'b11 ;
+  parameter ST_RESET = 2'b11 ;
 
   initial begin
     //initial reset
