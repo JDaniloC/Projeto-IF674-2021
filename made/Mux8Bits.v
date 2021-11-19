c81e09d6f6b1338e911b2a2edf4c087c17c8e071
Sofiamdl <sml@cin.ufpe.br>
2021-11-18 22:11:44 -0300

    feature(base-project): fix mux8bit

--- made/Mux8Bits.v
+++ made/Mux8Bits.v
@@ -10,37 +10,5 @@ module Mux8Bits (
     input  wire [31:0] data_7,
     output wire [31:0] data_output
 );
-
-    parameter IS_DATA_0 = 3'b000;
-    parameter IS_DATA_1 = 3'b001;
-    parameter IS_DATA_2 = 3'b010;
-    parameter IS_DATA_3 = 3'b011;
-    parameter IS_DATA_4 = 3'b100;
-    parameter IS_DATA_5 = 3'b101;
-    parameter IS_DATA_6 = 3'b110;
-    parameter IS_DATA_7 = 3'b111;
-
-    reg [31:0] data;
- 
-    always @(*) begin
-        case(selector)
-            IS_DATA_0:
-                data = data_0;
-            IS_DATA_1:
-                data = data_1;
-            IS_DATA_2:
-                data = data_2;
-            IS_DATA_3:
-                data = data_3;
-            IS_DATA_4:
-                data = data_4;
-            IS_DATA_5:
-                data = data_5;
-            IS_DATA_6:
-                data = data_6;
-            IS_DATA_7: 
-                data = data_7;
-        endcase
-    end
-
+    assign data_output = selector[0] ? (selector[1] ?  ( selector[2] ? data_7 :data_3 ) : (selector[2] ? data_5 :data_1 )) : (selector[1] ? (selector[2] ? data_6 : data_2 ) : (selector[2] ? data_4 : data_0 ));
 endmodule
